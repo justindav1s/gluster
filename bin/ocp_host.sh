5913e9fa53cb2c3759f6a7801bfb1d706fe3d337
Justin Davis <justinndavis@gmail.com>
2018-06-13 12:18:20 +0100

    ansible playbooks

---
+++ bin/ocp_host.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+NAME=ocp01
+
+virt-install \
+-n $NAME \
+--description $NAME \
+--os-type=Linux \
+--os-variant=rhel7 \
+--ram=32768 \
+--vcpus=4 \
+--graphics none \
+--disk path=/data/gluster/${NAME}_root.img,bus=virtio,size=50 \
+--disk path=/data/gluster/${NAME}_storage.img,bus=virtio,size=200 \
+--location '/images/rhel-server-7.5-x86_64-dvd.iso' \
+--extra-args 'console=ttyS0,115200n8 serial' \
+--mac=52:54:00:91:5a:d8
