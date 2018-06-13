f45482e0697795ac3acb346d9ea645b225a7271c
Justin Davis <justinndavis@gmail.com>
2018-06-13 18:10:47 +0100

    playbooks

--- ocp_install/makes_admin.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-
-oc login -u system:admin
-
-sleep 1
-
-oc adm policy add-cluster-role-to-user cluster-admin justin
--- ocp_install/uninstall_ocp.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-
-ansible-playbook  -i hosts.v3.9.localhost \
-    /usr/share/ansible/openshift-ansible/playbooks/adhoc/uninstall.yml
