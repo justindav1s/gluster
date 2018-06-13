43a9b426ecdb5d89cef6011d7e6cc7a9e0de500a
Justin Davis <justinndavis@gmail.com>
2018-06-13 15:00:17 +0100

    ansible playbooks

--- bin/gluster01.sh
+++ bin/gluster01.sh
@@ -9,11 +9,11 @@ virt-install \
 --description $NAME \
 --os-type=Linux \
 --os-variant=rhel7 \
---ram=4096 \
+--ram=8192 \
 --vcpus=2 \
 --graphics none \
 --disk path=/data/gluster/${NAME}_root.img,bus=virtio,size=10 \
---disk path=/data/gluster/${NAME}_storage.img,bus=virtio,size=100 \
+--disk path=/data/gluster/${NAME}_storage.img,bus=virtio,size=200 \
 --location '/images/rhel-server-7.5-x86_64-dvd.iso' \
 --extra-args 'console=ttyS0,115200n8 serial' \
 --mac=52:54:00:91:1a:d8 
