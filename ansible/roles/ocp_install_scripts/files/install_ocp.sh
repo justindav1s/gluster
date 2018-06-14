697143b2bb91daf0abff81cbf481d38aa8e64b27
Justin Davis <justinndavis@gmail.com>
2018-06-14 10:34:19 +0100

    playbooks

--- ansible/roles/ocp_install_scripts/files/install_ocp.sh
+++ ansible/roles/ocp_install_scripts/files/install_ocp.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
-
-ansible-playbook -i hosts.with.gluster \
-   /usr/share/ansible/openshift-ansible/playbooks/prerequisites.yml
-
 ansible-playbook -i hosts.with.gluster \
    /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml
