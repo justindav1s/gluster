9c92ed374642bf843e9e83fdce669173908c2e53
Justin Davis <justinndavis@gmail.com>
2018-06-18 17:34:40 +0100

    playbooks

--- bin/ocpcluster01.sh
+++ bin/ocpcluster01.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-NAME=ocp02
+NAME=ocpcluster01
 
 virt-install \
 -n $NAME \
 --description $NAME \
 --os-type=Linux \
 --os-variant=rhel7 \
---ram=32768 \
---vcpus=4 \
+--ram=16384 \
+--vcpus=2 \
 --graphics none \
 --disk path=/data/gluster/${NAME}_root.img,bus=virtio,size=20 \
---disk path=/data/gluster/${NAME}_docker_storage.img,bus=virtio,size=100 \
---disk path=/data/gluster/${NAME}_gluster_storage.img,bus=virtio,size=100 \
+--disk path=/data/gluster/${NAME}_docker_storage.img,bus=virtio,size=50 \
+--disk path=/data/gluster/${NAME}_gluster_storage.img,bus=virtio,size=50 \
 --location '/images/rhel-server-7.5-x86_64-dvd.iso' \
 --extra-args 'console=ttyS0,115200n8 serial' \
 --mac=52:54:00:91:6a:d8
